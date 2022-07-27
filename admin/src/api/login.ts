252dca6f874ea8c1ac30ba33eb432832544857d5
1006416402@qq.com <1006416402@qq.com>
2022-07-27 10:20:52 +0800

    删除多余

--- admin/src/api/login.ts
+++
@@ -1,9 +0,0 @@
-import { request } from './request'
-
-export async function login(data) {
-    return request({
-        url: '/admin/login',
-        method: 'POST',
-        data,
-    })
-}
--- admin/src/utils/checkLogin.tsx
+++
@@ -1,3 +0,0 @@
-export default function checkLogin() {
-  return !!localStorage.getItem('token'); //确保值为布尔值
-}
